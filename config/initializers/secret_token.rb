582c64d6463a55923cd790cce68a40273949cd2a
Bryan Chaiklin <bcchaiklin@gmail.com>
2011-12-30 12:27:09 -0500

    Starting Chill And Grill Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChillAndGrill::Application.config.secret_token = '26c5c3c6569d4194654b7531a6cf5f7424a14743f24314f89cf7ac1d4bf209d15d5256f63fa2be90f2da2f230f32531b0a20ebf67e80a31f05d807f851b8b458'
