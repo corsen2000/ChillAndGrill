09a32a56097a44c846cdc6cb668804a6911e05de
Bryan Chaiklin <bcchaiklin@gmail.com>
2011-12-31 15:38:13 -0500

    Working on user editing.  I think I should setup a different form for changing password, due to complications.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,22 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :current_password
+  attr_accessor :current_password
   has_secure_password
-  validates_presence_of :email, :first_name, :last_name, :on => :create
+  validates_presence_of :email, :first_name, :last_name
+  #validates_presence_of :password, :password_confirmation, :on => :update
+  validate :confirm_current_password, :on => :update
 
   def self.authenticate(user_id)
     User.find(user_id)
   end
+
+  def confirm_current_password
+    if authenticate(self.current_password) || self.password.blank?
+      true
+    else
+      errors.add(:current_password, " is not correct")
+      false
+    end
+  end
+
 end
