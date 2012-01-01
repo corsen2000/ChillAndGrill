1a5b4c6b2c58b20e34edc7fd5aaee553d4186980
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-01-01 15:30:26 -0500

    The current_password check was previously broken.  We need to fetch the old model from the database, and validate against that.  This is because the password is already changed in the current model though not saved.  Running the validation against the proposed model will always fail unless setting the same password.  We really want to compare against the model from the database, which we are now doing.
    We also clean up the logic a bit by making it a conditional validation, on :changing_password.  This implementation of this simply checks to see if the password attribute is blank or not.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,23 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :email, :first_name, :last_name
   validates :email, :email => true, :uniqueness => true
-  validate :confirm_current_password, :on => :update
+  validate :confirm_current_password, :on => :update, :if => :changing_password
 
   def self.authenticate(user_id)
     User.find(user_id)
   end
 
   def confirm_current_password
-    if authenticate(self.current_password) || self.password.blank?
+    if User.find(self).try(:authenticate, @current_password)
       true
     else
       errors.add(:current_password, " is not correct")
       false
     end
+  end
+
+  def changing_password
+    not @password.blank?
   end
 
 end
