214819916b23c8f3dee4da60f148ae07198298ab
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-01-08 11:10:59 -0500

    Adding a field roles_mask to the database for users.  Adding the ability to create roles from the users form.
    Next we need to modify this so that only admins can modify roles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :current_password
+  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :current_password, :roles
   attr_accessor :current_password
+
   has_secure_password
+
   validates_presence_of :email, :first_name, :last_name
   validates :email, :email => true, :uniqueness => true
   validate :confirm_current_password, :on => :update, :if => :changing_password
+
+  scope :with_role, lambda { |role| where("roles_mask & #{2**ROLES.index(role.to_s)} > 0")}
+
+  ROLES = %w[admin moderator guest]
 
   def self.authenticate(user_id)
     User.find(user_id)
@@ -21,6 +27,14 @@ class User < ActiveRecord::Base
 
   def changing_password
     not @password.blank?
+  end
+
+  def roles=(roles)
+    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.sum
+  end
+
+  def roles
+    ROLES.reject { |r| ((roles_mask || 0) & 2**ROLES.index(r)).zero? }
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111231172055) do
+ActiveRecord::Schema.define(:version => 20120108144210) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20111231172055) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "roles_mask"
   end
 
 end
