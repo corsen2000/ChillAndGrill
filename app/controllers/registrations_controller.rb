4108cd0c7c53360df438fe48b365592a33b6f6c8
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-01-22 13:44:11 -0500

    Adding flash messages for register / unregister on index page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < ApplicationController
     @registration = @event.registrations.build(:user => current_user)
     if @registration.save
       if request.referer == events_url
-        redirect_to events_path
+        redirect_to events_path, notice: 'Registration successful.'
       else
         redirect_to event_path(@event), notice: 'Registration successfull.'
       end
@@ -19,7 +19,7 @@ class RegistrationsController < ApplicationController
     @registration = @event.registrations.find(params[:id])
     @registration.destroy
     if request.referer == events_url
-      redirect_to events_path
+      redirect_to events_path, notice: 'Un-Registered'
     else
       redirect_to event_path(@event), notice: 'Un-Registered'
     end
