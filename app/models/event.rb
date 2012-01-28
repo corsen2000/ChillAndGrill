9d0999b4435ce37fa56a935402e345844437cd40
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-01-28 11:35:44 -0500

    Adding scopes for the Event mode: todays -> Get all events that are occuring today past   -> Get all events that occured in the past future -> Get all events that will occur in the future

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   validates_presence_of :title, :description, :start
   has_many :registrations
   has_many :users, :through => :registrations
-
-  # Event.where('start BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day)
-
+  scope :todays, where(:start => DateTime.now.beginning_of_day..DateTime.now.end_of_day)
+  scope :past, where("start < ?", DateTime.now.beginning_of_day)
+  scope :future, where("events.start > ?", DateTime.now.end_of_day)
 end
