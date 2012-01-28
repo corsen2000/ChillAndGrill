6c190dda572ce8a27fa3198c21ed57bbc04cbc67
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-01-28 11:39:46 -0500

    Wrapping the event scopes in lambda's so the call to DateTime.now is already accurate

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   validates_presence_of :title, :description, :start
   has_many :registrations
   has_many :users, :through => :registrations
-  scope :todays, where(:start => DateTime.now.beginning_of_day..DateTime.now.end_of_day)
-  scope :past, where("start < ?", DateTime.now.beginning_of_day)
-  scope :future, where("events.start > ?", DateTime.now.end_of_day)
+  scope :todays, lambda { where(:start => DateTime.now.beginning_of_day..DateTime.now.end_of_day) }
+  scope :past, lambda { where("start < ?", DateTime.now.beginning_of_day) }
+  scope :future, lambda { where("events.start > ?", DateTime.now.end_of_day) }
 end
