5280a2f5af0bd0d88cab08b1e16223cbccd7eb47
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-01-30 19:12:27 -0500

    Adding ordering to our todays,future, and past scopes. Todays and Future will be sorted Ascending, so that we see the most relevant first.  Most relavent is the one coming first. Past will be sorted Descending for the same reason.  The most relavent event in the past, is the last one.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   validates_presence_of :title, :description, :start
   has_many :registrations
   has_many :users, :through => :registrations
-  scope :todays, lambda { where(:start => DateTime.now.beginning_of_day..DateTime.now.end_of_day) }
-  scope :past, lambda { where("start < ?", DateTime.now.beginning_of_day) }
-  scope :future, lambda { where("events.start > ?", DateTime.now.end_of_day) }
+  scope :todays, lambda { where(:start => DateTime.now.beginning_of_day..DateTime.now.end_of_day).order("start") }
+  scope :past, lambda { where("start < ?", DateTime.now.beginning_of_day).order("start DESC") }
+  scope :future, lambda { where("events.start > ?", DateTime.now.end_of_day).order("start") }
 end
