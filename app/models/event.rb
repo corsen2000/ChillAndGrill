e7c8d768d880f2587b81ccd2cb939e586ad93a36
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-02-04 10:15:59 -0500

    Adding a label on the details page that shows the users rsvp status

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,8 @@ class Event < ActiveRecord::Base
   scope :todays, lambda { where(:start => DateTime.now.beginning_of_day..DateTime.now.end_of_day).order("start") }
   scope :past, lambda { where("start < ?", DateTime.now.beginning_of_day).order("start DESC") }
   scope :future, lambda { where("events.start > ?", DateTime.now.end_of_day).order("start") }
+
+  def rsvp_for_user(user)
+    rsvps.where(:user_id => user).first
+  end
 end
