cc07da9826f0b3399d617f427425542cac9b480b
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-02-25 16:33:53 -0500

    We don't need these anymore.  Event should have very little to do with notifications

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,6 @@ class Event < ActiveRecord::Base
   has_many :invitations
   has_many :notifications
   has_many :invited_users, :through => :invitations, :source => :user, :before_remove => proc { raise ActiveRecord::Rollback.new}
-  has_many :notified_users, :through => :notifications, :source => :user
-  has_many :well_notified_users, :through => :notifications, :source => :user, :conditions => proc { {"notifications.sent_for_event_version" => updated_at} }
 
   validates_presence_of :title, :description, :start
   validate :event_cannot_be_made_private, :on => :update
