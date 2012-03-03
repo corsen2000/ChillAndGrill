7d744e9c0f4ed12d854b5d3a65a75d3ae202ae9a
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-03-03 11:00:06 -0500

    This might work?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   force_ssl :only => [:new, :create]
 
   def index
-    redirect_to root_url
   end
 
   def show
@@ -19,7 +18,7 @@ class UsersController < ApplicationController
   def create
     authorize! :assign_roles, @user if params[:user][:roles]
     if @user.save
-      redirect_to root_url, :notice => 'Account Created.  Plase check your email for instructions.'
+      redirect_to root_url(:protocol => "http"), :notice => 'Account Created.  Plase check your email for instructions.'
     else
       render action: "new"
     end
