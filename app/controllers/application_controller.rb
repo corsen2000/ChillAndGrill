8a51cb3cac4d6ea18fc5f65de14726b2c27c82fb
Bryan Chaiklin <bcchaiklin@gmail.com>
2012-03-03 10:09:01 -0500

    This is bad, but I don't know how to test ssl locally, submitting this to run a test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :ensure_protocol
+
   include SessionHelper
 
   rescue_from CanCan::AccessDenied do
     flash[:error] = "Access Denied!"
     redirect_to root_url
   end  
+
+  def ensure_protocol  	
+  	if request.ssl? && !(self.class.read_inheritable_attribute(:force_ssl) || []).include?(action_name.to_sym)
+  		flash.keep
+  		redirect_to "http://" + request.host + request.request_uri
+  	end
+  end
 end
